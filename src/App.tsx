6e7f375ec9678ee2585ece307572309f713cad65
Gabriel Cruz <gaboxlive@gmail.com>
2025-10-22 11:58:24 -0600

    navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Providers from './Providers';
 import AppRoutes from './routes/indext';
 import './App.css';
+import NavigationBar from './components/nav/NavigationBar';
 
 const App: React.FC = () => {
     return (
         <Providers>
+            <NavigationBar />
             <div className='mt-5 mb-5 w-9/12 mx-auto'>
                 <AppRoutes />
             </div>
---
+++ src/components/nav/NavigationBar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import pokeball from '../../assets/pokeball.png';
+
+const NavigationBar: React.FC = () => (
+    <nav className='w-full bg-yellow-400 flex justify-between h-12 items-center shadow-lg'>
+        <div className='mx-auto flex justify-between items-center w-9/12'>
+            <Link to={'/'}>
+                <img src={pokeball} alt="Poke logo" className='w-10 h-10' />
+            </Link>
+            <div className='flex gap-5 items-center'>
+                <Link to={'/'} className='text-black hover:text-white'>
+                    Pokedex
+                </Link>
+                <Link to={'/favorite'} className='text-black hover:text-white'>
+                    Favoritos
+                </Link>
+            </div>
+        </div>
+    </nav>
+);
+
+export default NavigationBar;
